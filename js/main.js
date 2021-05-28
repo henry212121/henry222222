62f40eceed5162c977f1bb805d68915950eb0c6c
henry212121 <henrydelosreyes21@gmail.com>
2021-05-28 14:04:16 +0800

    dasjdk

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-/* comment */
+/* comment 
+
+
+alert("Are you ready for this AWESOME Website?");
+alert("Here it is");
+alert("Are you really sure about this?");
+alert("No Hesitation?");
+alert("But for real though?");
+alert("aight here you go");
+*/
